257bdc9bc7f651ce8f108707858d76a08bd1852b
Lucas Cuitino <lac422@scarletmail.rutgers.edu>
2021-04-15 22:49:21 -0400

    Cookie can now be used in testing

--- index.test.js
+++ index.test.js
@@ -2,17 +2,34 @@ const index = require("./index"); //import file we are testing'
 const app = index.app;
 const mongoose = index.mongoose;
 const request = require('supertest')(app); // supertest is a framework that allows to easily test web apis
+var session = require('supertest-session');
 
-test("Logs in a valid user", async (done) => {
-  await request.post('/login')
-  .type('form')
-  .send({
-      email: "user@rutgers.edu",
-      password: "rutgers"
+var testSession = null;
+
+describe('User session', () => {
+
+  it('Login', async(done) => {
+    await request.post('/login')
+    .type('form')
+    .send({
+        email: "user@rutgers.edu",
+        password: "rutgers"
+    })
+    .expect(302)
+    .expect('Location', /home/)
+    .then ((response) => {
+      this.Cookies = response.headers['set-cookie'].pop().split(';')[0];
+    })
+    done()
   })
-  .expect(302)
-  .expect('Location', /home/)
-  done()
+
+  it('Load home page as logged in user', async(done) => {
+    var req = request.get('/home')
+    req.cookies = this.Cookies;
+    await req
+    .expect(200)
+    done()
+  })
 })
 
 afterAll(done => {
